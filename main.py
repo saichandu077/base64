e784df2d31591fc6fe7d1bcb92cce6803be432af
Saichandu <saichanduboddula5@gmail.com>
2026-03-31 18:11:19 +0530

    Add FastAPI endpoint to encode image to base64

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+from fastapi import FastAPI, UploadFile, File
+import base64
+from mangum import Mangum
+
+app = FastAPI()
+
+@app.post("/encode-image")
+async def encode_image(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    encoded_string = base64.b64encode(image_bytes).decode("ascii")
+    return {"filename": file.filename, "base64": encoded_string}
+
+handler = Mangum(app)
